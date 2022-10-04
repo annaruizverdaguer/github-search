5d5bbf187f8b2658072ac9e246922fa5840bae0a
annaruizverdaguer <anna.ruiz@wallapop.com>
2022-10-04 18:00:23 +0200

    ja ho tindriem :D
    bueno bueno bueno, així que Montse i Meritxell

--- src/app.js
+++ src/app.js
@@ -1,23 +1,88 @@
-function noResults() {
-  let div = document.getElementsByClassName("results-container")[0];
+function noResults(type) {
+  let div = document.getElementsByClassName(`${type}-container`)[0];
   div.innerHTML = `
         <div class="media">
         <img src="media/cat-sad-cute-kitten.gif" class="no-results" alt="no results kitty" />
-        <p>This query returned no results </br> :(</p>
+        <p>This query returned no ${type} </br> :(</p>
       </div>
     `;
 }
 
-function printResults(results) {
-  // TODO
-  console.log(results);
+function printRepo(repo) {
+  let repoDiv = document.createElement("div");
+  repoDiv.className = "repo-container";
+
+  //title
+  let nameDiv = document.createElement("div");
+  nameDiv.className = "repo-name";
+  let p = document.createElement("p");
+  p.textContent = repo.name;
+  nameDiv.appendChild(p);
+  repoDiv.appendChild(nameDiv);
+
+  //icons
+  let iconDiv = document.createElement("div");
+  iconDiv.className = "icon-container";
+
+  //forks
+  let forkIcon = document.createElement("i");
+  forkIcon.className = "fa-solid fa-code-fork";
+  forkIcon.textContent = " " + repo.forks;
+  iconDiv.appendChild(forkIcon);
+
+  //stars
+  let starIcon = document.createElement("i");
+  starIcon.className = "fa-solid fa-star";
+  starIcon.textContent = " " + repo.stargazers_count;
+  iconDiv.appendChild(starIcon);
+
+  repoDiv.appendChild(iconDiv);
+  repoDiv.appendChild(document.createElement("hr"));
+  return repoDiv;
+}
+
+function printAllRepos(repos) {
+  if (repos.length > 0) {
+    let reposDiv = document.getElementsByClassName("repos-container")[0];
+    reposDiv.innerHTML = ``;
+    repos.forEach((repo) => {
+      let repoDiv = printRepo(repo);
+      reposDiv.appendChild(repoDiv);
+    });
+  } else {
+    noResults("repos");
+  }
+}
+
+function printUserInfo(user) {
+  let userDiv = document.getElementsByClassName("results-container")[0];
+  userDiv.innerHTML = ``;
+
+  // avatar de l'usuari + nom de l'usuari + descripció de l'usuari bio
+  let userBio = user.bio == null ? "This user does not have a bio." : user.bio;
+  userDiv.innerHTML = `<div class="user-avatar">
+        <img src="${user.avatar_url}" alt="User avatar" />
+        </div>
+        <div class="user-info">
+        <h3>${user.name}</h3>
+        <p>@${user.followers} followers</p>
+        <p>${userBio}</p>
+        </div>
+        <hr />`;
+}
+
+function printResults(results, repos) {
+  printUserInfo(results);
+  printAllRepos(repos);
 }
 
 function renderLogic(results) {
   if (results.message == "Not Found") {
-    noResults();
+    noResults("results");
   } else {
-    printResults(results);
+    fetch(results.repos_url)
+      .then((response) => response.json())
+      .then((repos) => printResults(results, repos));
   }
 }
 
